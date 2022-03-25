e7763e0518db52622ef582ecdd2dd7178edb17ba
Rajkumar Gupta <rajkumar@flairlabs.com>
2022-03-25 23:07:49 +0530

    Added Weather app's build files

---
+++ precache-manifest.9ebdd175fdd98e75b23aab4912583c67.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e95315ee551ac4a490d001af175eb5c8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bd8a4b662ce4cb8b4406",
+    "url": "./static/css/main.6ca484e5.chunk.css"
+  },
+  {
+    "revision": "fdec00128b370d3fde97",
+    "url": "./static/js/2.6ee728bc.chunk.js"
+  },
+  {
+    "revision": "a1900ba4d59359dcdaa937aadbf8af0d",
+    "url": "./static/js/2.6ee728bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd8a4b662ce4cb8b4406",
+    "url": "./static/js/main.a5525460.chunk.js"
+  },
+  {
+    "revision": "d4e3814c8d68168b58ad",
+    "url": "./static/js/runtime-main.82e0b1f1.js"
+  },
+  {
+    "revision": "6eb21da79303a35df1750607558ebeae",
+    "url": "./static/media/bg.6eb21da7.svg"
+  },
+  {
+    "revision": "47f1e6bdd7af84a534875c40ed81aac6",
+    "url": "./static/media/cloudy.47f1e6bd.svg"
+  },
+  {
+    "revision": "2ae50b330e08793b35ee4a5e729df69e",
+    "url": "./static/media/darkBg.2ae50b33.svg"
+  },
+  {
+    "revision": "5f81c3a6815e7834163e83875312feb8",
+    "url": "./static/media/github.5f81c3a6.svg"
+  },
+  {
+    "revision": "0f8ede749e5dc61255d557c6ffaf9a46",
+    "url": "./static/media/haze.0f8ede74.svg"
+  },
+  {
+    "revision": "17e40bd5c94c2cbedac31d7d7de613d1",
+    "url": "./static/media/heavy-rain.17e40bd5.svg"
+  },
+  {
+    "revision": "931f8d0e10c490b2770cd939a3f68802",
+    "url": "./static/media/high-icon.931f8d0e.svg"
+  },
+  {
+    "revision": "43a903ed0bfd53764b61806544b02860",
+    "url": "./static/media/humidity-icon.43a903ed.svg"
+  },
+  {
+    "revision": "948b91fd4fa54ebd87a2082571678dfd",
+    "url": "./static/media/location-icon.948b91fd.svg"
+  },
+  {
+    "revision": "4cadc629aa2ffd7dabafb0df86b890fb",
+    "url": "./static/media/low-icon.4cadc629.svg"
+  },
+  {
+    "revision": "c2e06f0212a9f4db46120dcf7ae2cc1e",
+    "url": "./static/media/partly-cloudy.c2e06f02.svg"
+  },
+  {
+    "revision": "2284050a3cba90029fd95ab123b2b0b8",
+    "url": "./static/media/pressure-icon.2284050a.svg"
+  },
+  {
+    "revision": "0138fd80e586127094a2058ae6cfd5d7",
+    "url": "./static/media/rain.0138fd80.svg"
+  },
+  {
+    "revision": "aff8f2a35e11e2c00e548e3c6c2f5207",
+    "url": "./static/media/search-icon.aff8f2a3.svg"
+  },
+  {
+    "revision": "4a7dd48bb6842dfd0f5d6f4d339ac651",
+    "url": "./static/media/sleet.4a7dd48b.svg"
+  },
+  {
+    "revision": "f6477891c68a906fcc6091c69b8adc4d",
+    "url": "./static/media/snow.f6477891.svg"
+  },
+  {
+    "revision": "3402daa5b40fff8cc27934f5b4fe9f6d",
+    "url": "./static/media/sunny.3402daa5.svg"
+  },
+  {
+    "revision": "d0dcc540816c0878adc943b4fcb54ba9",
+    "url": "./static/media/thunderstorm.d0dcc540.svg"
+  },
+  {
+    "revision": "7004d253811665b97b8f072088ae6de9",
+    "url": "./static/media/wind-icon.7004d253.svg"
+  }
+]);
